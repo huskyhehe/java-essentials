0a402a5c33b9f9a906fd4961f5d158cf101561c1
huskyhehe <zhou.he1@northeastern.edu>
2023-05-03 23:13:57 -0700

    Delete Lambda

--- Lambda/src/GenericInterface.java
+++
@@ -1,4 +0,0 @@
-
-public interface GenericInterface<T> {
-    T func(T t);
-}
--- Lambda/src/SAMInterface.java
+++
@@ -1,5 +0,0 @@
-// define a SAM Functional Interface in java
-@FunctionalInterface
-public interface SAMInterface {
-    String hello(String name);      // the single abstract method
-}
